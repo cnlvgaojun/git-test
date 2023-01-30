14bd1ccd5d7d10287fd595efa9e95e021b89e5cf
lvgaojun <2471603075@qq.com>
2023-01-30 17:15:58 +0800

    second commit

--- src/main/java/cn/lvgaojun/gittest.java
+++ src/main/java/cn/lvgaojun/gittest.java
@@ -3,5 +3,6 @@ package cn.lvgaojun;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hell git...");
+        System.out.println("hello git1...");
     }
 }
