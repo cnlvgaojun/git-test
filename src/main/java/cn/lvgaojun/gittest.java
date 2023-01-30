6f34aaf34f8dc756a1ab56921c0202e5837d9333
cnlvgaojun <123931812+cnlvgaojun@users.noreply.github.com>
2023-01-30 18:52:44 +0800

    Update gittest.java
    pull commit

--- src/main/java/cn/lvgaojun/gittest.java
+++ src/main/java/cn/lvgaojun/gittest.java
@@ -7,5 +7,6 @@ public class gittest {
         System.out.println("hello git3...");
         System.out.println("hello git4...");
         System.out.println("hello master test");
+        System.out.println("hello pull....");
     }
 }
